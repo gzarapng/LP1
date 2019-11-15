7db5c6343c9983f226fdaf9f3a9a8f5dbcf6cc9f
gzarapng <zara.gabriel@aluno.ifsp.edu.br>
2019-11-14 22:17:09 -0200

    update

---
+++ bbb/SQL_db_rh.sql
@@ -0,0 +1,49 @@
+--
+-- Base de Dados: `db_rh`
+--
+CREATE DATABASE IF NOT EXISTS `db_rh` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `db_rh`;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tb_func`
+--
+
+CREATE TABLE IF NOT EXISTS `tb_func` (
+  `MATRICULA` int NOT NULL AUTO_INCREMENT,
+  `NOME` varchar(40) NOT NULL,
+  `SETOR` varchar(25) NOT NULL,
+  `NASCIMENTO` date NOT NULL,
+  `SEXO` char NOT NULL,
+  `ESTAGIO` boolean NOT NULL,
+  `SALARIO` float NOT NULL,
+  PRIMARY KEY (`MATRICULA`)
+) DEFAULT CHARSET=utf8;
+
+-- ------------------------
+
+
+--
+-- Estrutura da tabela `tb_he`
+--
+
+CREATE TABLE IF NOT EXISTS `tb_he` (
+  `REG` int NOT NULL AUTO_INCREMENT,
+  `MATRICULA` int NOT NULL,
+  `DATA_HE` date NOT NULL,
+  `QTD_HORAS` int NOT NULL,
+  PRIMARY KEY (`REG`),
+  FOREIGN KEY (`MATRICULA`) REFERENCES tb_func(`MATRICULA`)
+) DEFAULT CHARSET=utf8;
+
+-- ------------------------
+
+--
+-- Usuário BD
+-- 
+
+CREATE USER IF NOT EXISTS 'aluno'@'localhost' IDENTIFIED BY 'aluno';
+
+
+GRANT ALL PRIVILEGES ON `db_rh` . * TO 'aluno'@'localhost';
